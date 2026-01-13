c2a73bfe619c9f67ff8598aa184388a69123fc29
peizhong <1647845408@qq.com>
2026-01-13 09:49:52 +0800

    revise

---
+++ lab1/hello_xiao/.pio/build/seeed_xiao_esp32c3/FrameworkArduino/cbuf.cpp.d
@@ -0,0 +1,3 @@
+.pio/build/seeed_xiao_esp32c3/FrameworkArduino/cbuf.cpp.o: \
+ C:/Users/Gao\ Peizhong/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.cpp \
+ C:/Users/Gao\ Peizhong/.platformio/packages/framework-arduinoespressif32/cores/esp32/cbuf.h
